aa8dd241aa8d2ac178f519f2bcc5c7eb10512ea4
GabrielHMA01 <gabriel.martins@sou.inteli.edu.br>
2025-02-21 11:28:46 -0300

    feat: cria a lógica de rotas privadas

--- src/frontend/src/Router.jsx
+++ src/frontend/src/Router.jsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage.jsx";
 import LoginPage from "./pages/login.jsx";
-import NotFound from "./pages/notFound.jsx";
 import RegisterPage from "./pages/registerPage.jsx";
+import NotFound from "./pages/notFound.jsx";
+import PrivateRoute from "./components/privateRoute.jsx";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        
+        {/* Rotas protegidas */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+
+        {/* Rota para não encontrado */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
