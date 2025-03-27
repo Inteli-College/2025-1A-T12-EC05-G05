4d573d69c8e88e2a34e6189821ec77c9c6db68c3
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2025-03-26 22:40:27 -0300

    feat: adiciona o component de filtros

--- src/frontend/src/pages/Logs.jsx
+++ src/frontend/src/pages/Logs.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PageHeader from "../components/PageHeader";
-import "../styles/fitaMedicamentos.css";
+import "../styles/Logs.css";
 import GridTable from "../components/GridTable";
+import Filter from "../components/Filter";
 
 const dataLogs = [
     { nome: "Fita 2", descricao: "Supporting line text lorem ipsum dolor sit amet, consectetur.", tipo: "Fita de medicamento", status: "Esperando autorização" },
@@ -16,9 +17,10 @@ const dataLogs = [
 export default function Logs() {
 
     return (
-        <div className="fitaMedicamentos">
-            <div className="conteudo">
+        <div className="logs">
+            <div className="conteudo-logs">
                 <PageHeader title="Histórico" />
+                <Filter />
                 <GridTable title="Logs" data={dataLogs} route="/logs" />
             </div>
         </div>
