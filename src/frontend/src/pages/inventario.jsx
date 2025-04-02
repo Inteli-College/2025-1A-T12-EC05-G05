2e788df76f2f28c2768bc4f52adc423a4491473d
Davi Nascimento de Jesus <davinasc.dejesus@gmail.com>
2025-04-02 14:03:55 -0300

    feat: adiciona componente de bin

--- src/frontend/src/pages/inventario.jsx
+++ src/frontend/src/pages/inventario.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PageHeader from "../components/PageHeader";
+import Bin from "../components/Bin";
 import "../styles/Inventario.css";
 
 
 
 export default function Inventario() {
+  const [loading, setLoading] = useState(true);
  
     return (
         <div className="inventario">
          
             <div className="conteudo">
                 <PageHeader title="Inventário" />
-                <p>⚠️ Ops! Esta página ainda está em construção.</p>
+                <div className="historico-content">
+                  <Bin 
+                    medicamento="Paracetamol"
+                    validade="2024-12-31"
+                    lote="123456"
+                    quantidade="10"
+                  />
+                </div>
             
                 
             </div>
